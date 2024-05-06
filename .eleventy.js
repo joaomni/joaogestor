f81816874253a615f76d3890836f998e78df9dde
João Moreira <joaojpmoreira25@gmail.com>
2024-05-06 12:02:13 +0000

    feat: home

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,12 @@
 module.exports = function (eleventyConfig) {
+  const eleventyNavigationPlugin = require("@11ty/eleventy-navigation")
+
+  module.exports = function (eleventyConfig) {
+    eleventyConfig.addPlugin(eleventyNavigationPlugin)
+  }
+
   eleventyConfig.addPassthroughCopy("./src/css")
-  eleventyConfig.addPassthroughCopy("./src/js")
+  
   return {
     dir: {
       input: "src",
---
+++ src/about-joaopaulo.md
@@ -0,0 +1,6 @@
+---
+title: João Paulo Moreira - Sobre
+whatsapp: https://api.whatsapp.com/send?phone=5513991027026&text=Olá, gostária de agendar uma reunião sobre estratégias de marketing para minha empresa!
+---
+
+{% include "about.njk" %}
