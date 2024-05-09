93f3ad2fee2625a6a2d7d944f141c9f35d2a8f24
João Moreira <joaojpmoreira25@gmail.com>
2024-05-09 17:09:40 +0000

    feat: header google style

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,17 +2,11 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
-	},
+    extend: {
+      boxShadow: {
+        'google': '0 2px 6px #0000001f, inset 0 -1px #dadce0',
+      }
+    }
+  },
 	plugins: [],
 }
-
-// module.exports = {
-//   theme: {
-//     extend: {
-//       boxShadow: {
-//         'google': '0 2px 6px #0000001f, inset 0 -1px #dadce0',
-//       }
-//     }
-//   }
-// }
