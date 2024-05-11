94356d0c1b07730e2c8b484ff0068dfb51f53e9a
joaomni <joaojpmoreira25@gmail.com>
2024-05-11 18:34:24 -0300

    feat: responsive design

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,7 +5,10 @@ export default {
     extend: {
       boxShadow: {
         'google': '0 2px 6px #0000001f, inset 0 -1px #dadce0',
-      }
+      },
+      boxShadow: {
+        'card': '0 1px 2px 0 rgba(60, 64, 67, .3), 0 1px 3px 1px rgba(60, 64, 67, .15)'
+      } 
     }
   },
 	plugins: [],
