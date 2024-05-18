ba45e430691fc00421afc320cd2fe1fa9908789d
joaomni <joaojpmoreira25@gmail.com>
2024-05-18 16:43:41 -0300

    SEO

--- src/components/Schedule.tsx
+++ src/components/Schedule.tsx
@@ -178,7 +178,7 @@ export default function () {
 				</DialogContent>
 			</Dialog>
 
-			{isVisible && (
+			{!isVisible && (
 				<Alert
 					className={`${isVisible ? 'animate-fadeIn' : 'animate-fadeOut'}`}
 					onAnimationEnd={onAnimationEnd}
